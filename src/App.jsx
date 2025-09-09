0f6617ad2e8b5a608134e4c76d0cf6735d487a84
yukkuri <reityan.sakura0104@gmail.com>
2025-09-09 18:25:56 +0900

    Upgradeの分割

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import { useState } from 'react'
 import './App.css'
 import Mainbutton from './components/Mainbutton/Mainbutton'
 import Infomation from './components/Infomation/Infomation'
+import Upgrade from './components/Upgrade/Upgrade'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <main>
+    <main>
+      <>
         <Mainbutton count={count} setCount={setCount} />
         <Infomation count={count} />
-      </main>
-    </>
+        <Upgrade />
+      </>
+    </main>
   )
 }
 
