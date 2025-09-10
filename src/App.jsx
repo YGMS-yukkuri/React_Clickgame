e682b234b6abb098a8ef087c9a2311e391cc1d64
yukkuri <reityan.sakura0104@gmail.com>
2025-09-10 18:52:07 +0900

    コンポーネントに分割

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import { useState } from 'react'
 import './App.css'
 import Mainbutton from './components/Mainbutton/Mainbutton'
 import Infomation from './components/Infomation/Infomation'
-import Upgrade from './components/Upgrade/Upgrade'
+import DisplayUPG from './components/DisplayUPG/DisplayUPG';
+import Upgrade from './UpgradeIndex';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <main>
       <>
         <Mainbutton count={count} setCount={setCount} />
         <Infomation count={count} />
-        <Upgrade />
+        <DisplayUPG count={count} setCount={setCount} UpgradeIndex={Upgrade} />
       </>
     </main>
   )
