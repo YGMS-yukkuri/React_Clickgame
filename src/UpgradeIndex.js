303d160f6e009ed052b9546889cabdb863340d58
yukkuri <reityan.sakura0104@gmail.com>
2025-09-10 18:52:24 +0900

    アップグレードの内容を配列に格納

---
+++ src/UpgradeIndex.js
@@ -0,0 +1,6 @@
+let UpgradeIndex = [
+    {id: 1, name: "Upgrade 1", description: "Upgrade 1", cost: 10, "DisplayCount": 0 , "displayed": false},
+    {id: 2, name: "Upgrade 2", description: "Upgrade 2", cost: 20, "DisplayCount": 10 , "displayed": false},
+    {id: 3, name: "Upgrade 3", description: "Upgrade 3", cost: 30, "DisplayCount": 15 , "displayed": false}
+]
+export default UpgradeIndex;
