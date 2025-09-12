ef5bfaa416795a0a4f3f6586733a7cfa84c311ad
yukkuri <reityan.sakura0104@gmail.com>
2025-09-12 17:19:50 +0900

    デバッグ用でコストを減少、購入数や購入による効果などを配列に追加

--- src/UpgradeIndex.js
+++ src/UpgradeIndex.js
@@ -1,6 +1,36 @@
 let UpgradeIndex = [
-    {id: 1, name: "Upgrade 1", description: "Upgrade 1", cost: 10, "DisplayCount": 0 , "displayed": false},
-    {id: 2, name: "Upgrade 2", description: "Upgrade 2", cost: 20, "DisplayCount": 10 , "displayed": false},
-    {id: 3, name: "Upgrade 3", description: "Upgrade 3", cost: 30, "DisplayCount": 15 , "displayed": false}
+    {
+        id: 1, 
+        name: "Upgrade 1",
+        description: "Upgrade 1",
+        cost: 5,
+        amount: 0,
+        cps: 0,
+        clickup: 1,
+        DisplayCount: 0,
+        displayed: false
+    },
+    {
+        id: 2,
+        name: "Upgrade 2",
+        description: "Upgrade 2",
+        cost: 5,
+        amount: 0,
+        cps: 1,
+        clickup: 0,
+        DisplayCount: 10,
+        displayed: false
+    },
+    {
+        id: 3,
+        name: "Upgrade 3",
+        description: "Upgrade 3",
+        cost: 5,
+        amount: 0,
+        cps: 5,
+        clickup: 0,
+        DisplayCount: 15,
+        displayed: false
+    }
 ]
 export default UpgradeIndex;
