0a8af1c3fb2c9b3193bb00edbe2e1f0842c8fa33
yukkuri <reityan.sakura0104@gmail.com>
2025-09-16 23:23:37 +0900

    色が適切に表示されるように修正

--- src/components/Play/Main/Judgegoal/Judgegoal.jsx
+++ src/components/Play/Main/Judgegoal/Judgegoal.jsx
@@ -3,16 +3,19 @@ import styles from './Judgegoal.module.css'
 
 export default function Judgegoal({ goal, count }) {
 
-    useEffect(() => {
-        console.log(`Goal: ${goal}, Current Score: ${count}`);
-    }, [goal, count]);
+  useEffect(() => {
+    console.log(`Goal: ${goal}, Current Score: ${count}`);
+  }, [goal, count]);
 
   return (
     <div className={styles.Goal}>
-        <div className={styles.goalbar}>
-            <div className={styles.goalbarFill} style={{ width: `${(count / goal) * 100}%` }}></div>
-        </div>
-      <p>Judgegoal: {goal} Current Score: {count}</p>
+      <div className={styles.goalbar}>
+        <div className={styles.goalbarFill} style={{ height: `${(count / goal) * 100}%` }}></div>
+      </div>
+      <div className={styles.textbox}>
+        <a>クリア条件: {goal}以上</a>
+        <a>現在のスコア: {count}</a>
+      </div>
     </div>
   )
 }
