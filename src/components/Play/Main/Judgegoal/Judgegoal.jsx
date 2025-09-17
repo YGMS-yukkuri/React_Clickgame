0386e1de4b5bf1da938528501f5c85893217ca25
yukkuri <reityan.sakura0104@gmail.com>
2025-09-17 18:28:55 +0900

    クリア条件の表示ロジックを改善し、スタイルを調整

--- src/components/Play/Main/Judgegoal/Judgegoal.jsx
+++ src/components/Play/Main/Judgegoal/Judgegoal.jsx
@@ -1,21 +1,41 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './Judgegoal.module.css'
+import { Link } from 'react-router-dom';
 
 export default function Judgegoal({ goal, count }) {
 
+  const [isCleared, setIsCleared] = useState(false);
   useEffect(() => {
     console.log(`Goal: ${goal}, Current Score: ${count}`);
   }, [goal, count]);
 
-  return (
-    <div className={styles.Goal}>
-      <div className={styles.goalbar}>
-        <div className={styles.goalbarFill} style={{ height: `${(count / goal) * 100}%` }}></div>
+
+  if (goal <= count || isCleared) {
+    if (!isCleared) {
+      setIsCleared(true);
+    }
+    return (
+      <div className={styles.Goal}>
+        <div className={styles.goalbar}>
+          <div className={styles.goalbarFill} style={{ height: `${(count / goal) * 100}%` }}></div>
+        </div>
+        <div className={styles.textbox}>
+          <a>クリア条件: {goal}以上:達成！</a>
+          <Link to="/result" state={{ goal: goal, count: count }} className={styles.nextbutton}>
+          <button>リザルト画面へ</button></Link>
+        </div>
       </div>
-      <div className={styles.textbox}>
-        <a>クリア条件: {goal}以上</a>
-        <a>現在のスコア: {count}</a>
+    );
+  } else {
+    return (
+      <div className={styles.Goal}>
+        <div className={styles.goalbar}>
+          <div className={styles.goalbarFill} style={{ height: `${(count / goal) * 100}%` }}></div>
+        </div>
+        <div className={styles.textbox}>
+          <a>クリア条件: {goal}以上</a>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
